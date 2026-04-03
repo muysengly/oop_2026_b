537a5ab03bbb7602e5e36d10ba61b8ef6614c249
muysengly-itc <muy.sengly@itc.edu.kh>
2026-04-03 14:16:46 +0700

    dafsdf

--- Main.java
+++ Main.java
@@ -5,9 +5,12 @@ public class Main {
 
     int[] input = { 6, 7, 3, 4, 2, 1 };
 
-    Algorithm_V1 al = new Algorithm_V1(input);
+    Algorithm_V1 al = new Algorithm_V1();
 
-    System.out.println("Sum: " + al.sum());
+    al.sample;
+
+    System.out.println("Sum: " + al.sum(input));
+    System.out.println("Max: " + al.max(input));
 
   }
 }
--- gtr/Algorithm_V1.java
+++ gtr/Algorithm_V1.java
@@ -3,32 +3,41 @@ package gtr;
 public class Algorithm_V1 {
 
   // atributes mr_aaa
-  int[] sample = { 2, 4, 1, 6, 8 };
-  int[] input;
+  private int[] sample = { 2, 4, 1, 6, 8 };
 
   // constructor mr_bbb
-  public Algorithm_V1(int[] input) {
-    this.input = input;
+  public Algorithm_V1() {
+    //
   }
 
   // method sum mr_aaa
-  int sum() {
+  public int sum(int[] input) {
     int sum = 0;
     for (int i = 0; i < input.length; i++) {
       sum += input[i];
     }
+    System.out.println("Sum: " + sum);
     return sum;
   }
 
   // method max mr_aaa
-  int max() {
+  public int max(int[] input) {
     int max = input[0];
     for (int i = 1; i < input.length; i++) {
       if (input[i] > max) {
         max = input[i];
       }
     }
+    System.out.println("Max: " + max);
     return max;
+  }
+
+  public static void main(String[] args) {
+    Algorithm_V1 al = new Algorithm_V1();
+
+    al.sum(al.sample);
+    al.max(al.sample);
+
   }
 
 }
