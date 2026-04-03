eb047f19d43e41b7a3875eef4f7e87900729ae6a
muysengly-itc <muy.sengly@itc.edu.kh>
2026-04-03 13:45:58 +0700

    update

--- Algorithm_V1.java
+++ Algorithm_V1.java
@@ -1,10 +1,13 @@
 public class Algorithm_V1 {
 
   // atributes mr_aaa
+  int[] sample = { 2, 4, 1, 6, 8 };
   int[] input;
 
   // constructor mr_bbb
 
-  // method mr_aaa
+  // method sum mr_aaa
+
+  // method max mr_aaa
 
 }
